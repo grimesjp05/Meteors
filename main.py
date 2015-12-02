d758f0ff592350958ace85a87231dea7cf53a64a
grimesjp05 <grimesjp05@mounties.mansfield.edu>
2015-12-02 00:16:57 -0500

    changed space key to return key

--- main.py
+++ main.py
@@ -64,39 +64,39 @@ projs = []
 objs = pygame.sprite.Group([])
 
 player = Player(screen, projs)
-	
+
 #random asteroids in the background of the background!
 for i in range(0, 6):
 	objs.add(Asteroid(screen, asteroidImgs, 70, player, objs))
 
 #ZOMG GAME LOOP
 while running:
-	
+
 	ticks = clock.tick(50)
-	
+
 	#Menu Scene
 	if scene == 0:
-		
+
 		screen.blit(stars, (0,0))
-		
+
 		objs.update(ticks, screen)
-		
-		#I don't know a better way to handle basic texts... this class kinda sucks. 
+
+		#I don't know a better way to handle basic texts... this class kinda sucks.
 		texts = []
-		
+
 		texts.append(Text(screen, CENTER, (255,255,255,255), 50, "PYSTROIDS", .75*screen.get_height()/2))
 		texts.append(Text(screen, CENTER, (255,255,255,255), 20, "Press Space to Start", screen.get_height()/2))
 		texts.append(Text(screen, CENTER, (255,255,255,255), 15, "Press H to view High Scores", 1.75*screen.get_height()/2))
-	
-		
+
+
 		for t in texts:
 			t.update()
-		
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = 0
 			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_SPACE:
+				if event.key == pygame.K_RETURN:
 					objs = pygame.sprite.Group([])
 					font = pygame.font.Font(pygame.font.get_default_font(), 25)
 					points = 0
@@ -109,17 +109,17 @@ while running:
 					player = Player(screen, projs)
 				elif event.key == pygame.K_h:
 					scene = 3
-					
-	
+
+
 	#Game scene
 	elif scene == 1:
 		#Game Events
-		
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = 0
 			elif event.type == pygame.KEYUP or event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_UP or event.key == pygame.K_DOWN or event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT or event.key == pygame.K_SPACE:
+				if event.key == pygame.K_UP or event.key == pygame.K_DOWN or event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT or event.key == pygame.K_RETURN:
 					player.keyEvent(event)
 				elif event.key == pygame.K_ESCAPE:
 					running = 0
@@ -131,30 +131,30 @@ while running:
 			#points
 			elif event.type == POINTS:
 				points += event.points
-				
+
 			#player dies
 			elif event.type == PLAYERDEAD:
 				lives -= 1
-				
+
 		#update objects
 		screen.blit(stars, (0,0))
 		objs.update(ticks, screen)
 		player.update(ticks, screen, objs)
-		
+
 		#draw explosion particles
 		for i in explosions:
 			if i.life <= 0:
 				explosions.remove(i)
 			else:
 				i.update(ticks, screen)
-		
+
 		#check for projectile collisions
 		for proj in projs:
 			if proj.life <= 0:
 				projs.remove(proj)
 			else:
 				proj.update(ticks, screen)
-	
+
 		#check collisions
 		for obj in objs:
 			for proj in projs:
@@ -165,44 +165,44 @@ while running:
 					break
 			if dist(obj.pos, player.pos) < obj.radius + player.radius:
 				player.destroy()
-		
+
 		#update gui
 		screen.blit(font.render(str(points), 0, (255,255,255,255)), (5, 5))
 		for i in range(1, lives+1):
 			screen.blit(livesImg, (screen.get_width() - (i*livesImg.get_width()), 5))
-			
+
 		#signal all asteroids destroyed:
 		if len(objs) == 0:
 			pygame.event.post(pygame.event.Event(pygame.USEREVENT+1, {}))
-		
+
 		#signal extra life:
 		if points > 10000 * extraLives:
 			lives += 1
 			extraLives += 1
-		
+
 		#signal no lives left:
 		if lives == 0:
 			name = ""
 			scene = 2
-			
-	
+
+
 	#Game Over scene
 	elif scene == 2:
 		screen.blit(stars, (0,0))
 		objs.update(ticks, screen)
-		
+
 		#probably should write a class for this... Writes a few text lines to different places/sizes
 		#wanted to condense writing multiple centered texts at multiple sizes and location.  UGLY!
 		texts = []
-		
+
 		texts.append(Text(screen, CENTER, (255,255,255,255), 50, "GAME OVER", .75*screen.get_height()/2))
 		texts.append(Text(screen, CENTER, (255,255,255,255), 20, "Enter your Name", screen.get_height()/2))
 		texts.append(Text(screen, CENTER, (255,255,255,255), 15, name, 1.25*screen.get_height()/2))
 		texts.append(Text(screen, CENTER, (255,255,255,255), 20, "Score: "+str(points), 1.5*screen.get_height()/2))
-		
+
 		for t in texts:
 			t.update()
-		
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = 0
@@ -215,63 +215,52 @@ while running:
 					name = name[0:-1]
 				elif event.key < 123 and event.key >= 97:
 					name += pygame.key.name(event.key).upper()
-	
+
 	#High Scores scene
 	elif scene == 3:
 		screen.blit(stars, (0,0))
 		objs.update(ticks, screen)
-		
-		#I don't know a better way to handle basic texts... this class kinda sucks. 
+
+		#I don't know a better way to handle basic texts... this class kinda sucks.
 		texts = []
 		texts.append(Text(screen, CENTER, (255,255,255,255), 40, "High Scores", 20))
 		texts.append(Text(screen, CENTER, (255,255,255,255), 20, "Press Space to Return to the Menu", screen.get_height() - 50))
-		
+
 		for i in range(1, 11):
 			num = Text(screen)
 			num.setWords(str(i)+".")
 			num.setX(100 - num.font.size(num.words)[0])
 			num.setyPos(100+25*i)
 			texts.append(num)
-		
+
 		i = 1
 		for score in highScores.scores:
 			name = Text(screen)
 			points = Text(screen)
-			
+
 			name.setWords(score.name)
 			name.setX(150 - name.font.size(num.words)[0])
 			name.setyPos(100+25*i)
-			
+
 			points.setWords(str(score.score))
 			points.setAlignment(RIGHT)
 			points.setX(-100)
 			points.setyPos(100+25*i)
-			
+
 			texts.append(points)
 			texts.append(name)
 			i+= 1
-			
-		
+
+
 		for t in texts:
 			t.update()
-		
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = 0
 			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_SPACE:
+				if event.key == pygame.K_RETURN:
 					scene = 0
-	
-	
+
+
 	pygame.display.flip()
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
--- player.py
+++ player.py
@@ -10,18 +10,18 @@ from projectile import *
 
 class Player(movingObject):
 	def __init__(self, screen, projectiles):
-		
+
 		image = pygame.Surface((43,37), pygame.SRCALPHA)
-		
+
 		image.fill((0,0,0,0))
-		
+
 		pygame.draw.line(image, pygame.Color('white'), (21, 1), (7, 36), 2)
 		pygame.draw.line(image, pygame.Color('white'), (21, 1), (32, 36), 2)
 		pygame.draw.line(image, pygame.Color('white'), (10, 30), (30, 30), 2)
 		image = pygame.transform.rotozoom(image, -90, 1)
-		
+
 		movingObject.__init__(self, image, 300, 300, 18)
-		
+
 		self.rot = 90
 		self.mass = 10
 		self.alive = 1
@@ -34,7 +34,7 @@ class Player(movingObject):
 		self.respawnTime = 100
 		self.keyLeft = 0
 		self.keyRight = 0
-		
+
 	def keyEvent(self, event):
 		if self.alive:
 			if event.type == pygame.KEYDOWN:
@@ -44,13 +44,13 @@ class Player(movingObject):
 					self.keyLeft = self.rotSpeed
 				elif event.key == pygame.K_RIGHT:
 					self.keyRight = -self.rotSpeed
-				elif event.key == pygame.K_SPACE:
+				elif event.key == pygame.K_RETURN:
 					shipTip = create(self.rot, 22)
 					shipTip.y *= -1
 					shipTip += self.pos
 					if len(self.projs) < 6:
 						self.projs.append(Projectile(shipTip, self.rot, self.v, 4, 70))
-					
+
 			elif event.type == pygame.KEYUP:
 				if event.key == pygame.K_UP:
 					self.keyUp = 0
@@ -58,13 +58,13 @@ class Player(movingObject):
 					self.keyLeft = 0
 				if event.key == pygame.K_RIGHT:
 					self.keyRight = 0
-		
+
 	def update(self, ticks, screen, objs):
 		if self.alive:
 			if self.keyUp:
 				self.f = create(self.rot, self.thrust)
 				self.f.y *= -1
-				
+
 				if self.frame == 0:
 					self.frame = 1
 				else:
@@ -72,7 +72,7 @@ class Player(movingObject):
 			else:
 				self.f = Vector()
 				self.image = self.origImage
-			
+
 			self.angV = self.keyRight + self.keyLeft
 		else:
 			if self.respawnTime > 0:
@@ -84,23 +84,21 @@ class Player(movingObject):
 				else:
 					self.alive = 1
 					self.respawnTime = 100
-					
+
 					pygame.draw.line(self.image, pygame.Color('white'), (21, 1), (7, 36), 2)
 					pygame.draw.line(self.image, pygame.Color('white'), (21, 1), (32, 36), 2)
 					pygame.draw.line(self.image, pygame.Color('white'), (10, 30), (30, 30), 2)
 					self.image = pygame.transform.rotozoom(self.image, -90, 1)
 					self.origImage = self.image
-				
-			
+
+
 		movingObject.update(self, ticks, screen)
-		
+
 	def destroy(self):
 		if self.alive:
 			self.__init__(self.screen, self.projs)
 			self.image.fill((0,0,0,0))
 			self.alive = 0
-			
+
 			dead = pygame.event.Event(pygame.USEREVENT+3, pos = self.pos)
 			pygame.event.post(dead)
-			
-		
